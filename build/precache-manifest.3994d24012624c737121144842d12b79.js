d9f29b9f7cbeaca83cfb364fecc888bb30900cd0
Muhammad Hamad <hamadali77115033@gmail.com>
2024-01-17 10:32:52 +0500

    done

---
+++ build/precache-manifest.3994d24012624c737121144842d12b79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51ffd9ecc9be3900ecc3e65c9dccdaf1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6fed751ce1edce192ab5",
+    "url": "/static/css/2.7f1403ef.chunk.css"
+  },
+  {
+    "revision": "a011be111bacf4341f1e",
+    "url": "/static/css/main.10507661.chunk.css"
+  },
+  {
+    "revision": "6fed751ce1edce192ab5",
+    "url": "/static/js/2.22632cb3.chunk.js"
+  },
+  {
+    "revision": "35f1a3ca561e93b64bcba705f76bd30f",
+    "url": "/static/js/2.22632cb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a011be111bacf4341f1e",
+    "url": "/static/js/main.c69e3c1d.chunk.js"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  }
+]);
